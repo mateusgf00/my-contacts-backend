5f39761391f10054f3c2a8cc639278807605458b
mateus <mateusgf00@gmail.com>
2022-07-26 19:59:14 -0300

    refatoring method findAll, findById, findByEmail

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -1,36 +1,37 @@
 const { response } = require('express');
 const ContactsRepository = require('../repositories/ContactsRepository');
 
-class ContactController{
+class ContactController {
 
-    async index(request, response){
-       const contacts =  await ContactsReposotory.findAll();
+    async index(request, response) {
+        const { orderBy } = request.query;
+        const contacts = await ContactsRepository.findAll(orderBy);
 
-       response.json(contacts);
+        response.json(contacts);
     }
 
-    async show(request, response){
-        const {id} = request.params;
-        const contact = await ContactsReposotory.findById(id);
+    async show(request, response) {
+        const { id } = request.params;
+        const contact = await ContactsRepository.findById(id);
 
-        if(!contact){
-            return response.status(404).json({error: 'User not found'})
+        if (!contact) {
+            return response.status(404).json({ error: 'User not found' })
         }
 
         response.json(contact)
 
     }
-    async store(request, response){
+    async store(request, response) {
         const { name, email, phone, category_id } = request.body;
 
-        if(!name){
-            return response.status(400).json({error: "Name is required"});
+        if (!name) {
+            return response.status(400).json({ error: "Name is required" });
         }
 
         const contactExists = await ContactsRepository.findByEmail(email);
 
-        if(contactExists){
-            return response.status(400).json({error: "this email already exists"})
+        if (contactExists) {
+            return response.status(400).json({ error: "this email already exists" })
         }
 
         const contact = await ContactsRepository.create({
@@ -39,37 +40,37 @@ class ContactController{
 
         response.json(contact);
     }
-    async update(request, response){
+    async update(request, response) {
         const { id } = request.params;
         const { name, email, phone, category_id } = request.body;
 
         const contactExists = await ContactsRepository.findById(id);
-        if(!contactExists){
-            return response.status.json({error: 'User not found'});
+        if (!contactExists) {
+            return response.status.json({ error: 'User not found' });
         }
 
-        if(!name){
-            return response.status(400).json({error: "Name is required"});
+        if (!name) {
+            return response.status(400).json({ error: "Name is required" });
         }
 
         const emailExists = await ContactsRepository.findByEmail(email);
 
-        if(emailExists && emailExists.id !== id){
-            return response.status(400).json({error: "this email already exists"})
+        if (emailExists && emailExists.id !== id) {
+            return response.status(400).json({ error: "this email already exists" })
         }
 
-        const contact = await ContactsRepository.update(id,{ name, email, phone, category_id });
+        const contact = await ContactsRepository.update(id, { name, email, phone, category_id });
 
         response.json(contact);
 
     }
-    async delete(request, response){
-        const {id} = request.params;
+    async delete(request, response) {
+        const { id } = request.params;
 
         const contact = await ContactsRepository.findById(id);
 
-        if(!contact){
-            return response.status(404).json({error: 'User not found'})
+        if (!contact) {
+            return response.status(404).json({ error: 'User not found' })
         }
 
         await ContactsReposotory.delete(id);
