51fa3ad65fdceff3aa14fc637a557c8b6948e581
mateus <mateusgf00@gmail.com>
2022-07-29 14:18:57 -0300

    add error handler

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+require('express-async-errors')
+
 const routes = require('./routes');
 
 const app = express();
@@ -7,5 +9,11 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+// Middleware Error Handler
+app.use((error, request, response, next) =>{
+  console.log(error);
+  response.sendStatus(500);
+})
 
 app.listen(3000, () => console.log('Server started at http://localhost:3000'));
